b2aeab470ac0b898b2860cb43241c08e685d878b
noyonsarder <noyonsarder87@gmail.com>
2025-02-21 12:39:07 +0600

    Added failed test case

---
+++ tests/blazeLogin.spec.js
@@ -0,0 +1,17 @@
+const {test, expect} = require('@playwright/test');
+
+test("Navigate to PDP", async({page})=>{
+
+    await page.goto("https://demoblaze.com/index.html");
+    await page.waitForSelector(`//div[@class='card-block']`);
+    const title= await page.title();
+    await expect(title).toContain("STORE");
+})
+
+test("Failed Test Case", async({page})=>{
+
+    await page.goto("https://demoblaze.com/index.html");
+    await page.waitForSelector(`//div[@class='card-block']`);
+    const title= await page.title();
+    await expect(title).toContain("STORE");
+})
